274f8688b564487b4ba2adfe43a9b5d95a5bd9d5
fira011 <33779453+fira011@users.noreply.github.com>
2018-01-18 03:01:12 +0700

    Update Talk.py

--- KRIS/Api/Talk.py
+++ KRIS/Api/Talk.py
@@ -20,8 +20,8 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/7.18.0"#"Mozilla/5.0"
-  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
+  UA = "Mozilla/5.0"#"Mozilla/5.0"
+  LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
   #UA = "Line/7.18.0"#"Mozilla/5.0"
   #LA = "CHROMEOS\t.1.4.5\tChrome_HP\t1"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
   #UA = "Line/6.0.0 iPad4,1 9.0.2"
